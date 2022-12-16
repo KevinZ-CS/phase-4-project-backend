469bce5aafc9248ce67a40ff44948c1cba813683
Kevin Zheng <kevzheng27@gmail.com>
2022-12-15 20:34:34 -0500

    Add create and show action in Users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+rescue_from ActiveRecord::RecordInvalid, :render_unprocessable_entity_response
+
+before_action :authorize
+#putting before acction in application controller will run authorize for every controller inheriting from application controller
+
+private
+
+def authorize
+  @current_user = User.find_by(id: session[:user_id])
+
+  render json: { errors: ['Not authorized'] }, status: :unauthorized unless @current_user
+  #will return error unless current user is true(has a value except false or nil)
+end
+
+def render_unprocessable_entity_response(exception)
+  render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+end
+
 end
