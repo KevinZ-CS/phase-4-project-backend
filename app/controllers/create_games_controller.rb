cda243274a685d699bfbbacaa960a70e04134e95
Kevin Zheng <kevzheng27@gmail.com>
2022-12-15 22:15:02 -0500

    Add create action to create games controller

--- app/controllers/create_games_controller.rb
+++ app/controllers/create_games_controller.rb
@@ -1,2 +1,14 @@
 class Games::CreateGamesController < ApplicationController
+
+    def create
+        game = Game.create!(game_params)
+        render json: :games, status: :created
+    end
+
+    private
+
+    def game_params
+        params.permit(:title, :platform, :genre, :img_url, :price)
+    end
+
 end
