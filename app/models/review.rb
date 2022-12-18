59365e32f075a3f39ae68a9962fb046980c1a312
Kevin Zheng <kevzheng27@gmail.com>
2022-12-17 20:21:04 -0500

    Add validations for score and comment in Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :game
   belongs_to :user
+
+  validates :score, inclusion: 0..10
+  validates :comment, length: { minimum: 20 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,10 +13,10 @@ Game.create(title: "Mario Kart", platform: "Switch", genre: "Racing", img_url: '
 Game.create(title: "Pokemon Violet", platform: "Switch", genre: "RPG", img_url: 'https://assets-prd.ignimgs.com/2022/08/03/pokemon-violet-1659542326365.jpg', price: 60)
 Game.create(title: "Pokemon Scarlet", platform: "Switch", genre: "RPG", img_url: 'https://assets-prd.ignimgs.com/2022/08/03/pokemon-scarlet-1659542292648.jpg', price: 60)
 
-Review.create(score: rand(1..10), comment: Faker::Lorem.sentence, game_id: 1)
-Review.create(score: rand(1..10), comment: Faker::Lorem.sentence, game_id: 2)
-Review.create(score: rand(1..10), comment: Faker::Lorem.sentence, game_id: 3)
-Review.create(score: rand(1..10), comment: Faker::Lorem.sentence, game_id: 4)
-Review.create(score: rand(1..10), comment: Faker::Lorem.sentence, game_id: 5)
+# Review.create(score: rand(1..10), comment: Faker::Lorem.sentence, game_id: 1, user_id: 1)
+# Review.create(score: rand(1..10), comment: Faker::Lorem.sentence, game_id: 2, user_id: 1)
+# Review.create(score: rand(1..10), comment: Faker::Lorem.sentence, game_id: 3, user_id: 1)
+# Review.create(score: rand(1..10), comment: Faker::Lorem.sentence, game_id: 4, user_id: 1)
+# Review.create(score: rand(1..10), comment: Faker::Lorem.sentence, game_id: 5, user_id: 1)
 
 puts "🌱 Done seeding!"
